64bd4aceabd7f006c2f3ed4d5d3a1d5b573d7dcd
VishalKumarSain <vishaldhoria007@gmail.com>
2023-11-05 22:14:10 +0530

    second

--- getIP.js
+++ getIP.js
@@ -5,27 +5,14 @@ const getUserLocationInfo = async () => {
     var url = "https://api.ipify.org?format=json";
     const response = await fetch(url);
     const data = await response.json();
-    //     console.log("user ip info: ", data);
 
     if (data.ip) {
       const userData = await fetch(`https://ipapi.co/${data.ip}/json/`);
       const userInfo = await userData.json();
-      //       console.log("user location info: ", userInfo);
 
       if (userInfo.latitude && userInfo.longitude) {
         userLocation = userInfo;
-        // console.log("user location", userLocation);
         window.location.href = "location.html";
-        document.addEventListener("DOMContentLoaded", function () {
-          const showUserIP = document.getElementById("showUserIP");
-          console.log("showUserIP===", showUserIP);
-          showUserIP.innerText = "IP Address: " + data.ip;
-        });
-
-        if (!localStorage.getItem("hasRedirected")) {
-          localStorage.setItem("hasRedirected", "true");
-          // Redirect to location.html
-        }
       } else {
         console.log("Missing location information");
       }
