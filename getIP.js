106bcf4becbb6c2444bf5d00eb80a9e54e8c77e9
VishalKumarSain <vishaldhoria007@gmail.com>
2023-11-07 23:18:39 +0530

    filter

--- getIP.js
+++ getIP.js
@@ -1,9 +1,9 @@
 let userLocation = {};
 const getIPButton = document.getElementById("getIPButton");
-
-getIPButton.addEventListener("click", ()=>{
-  getIPButton.innerText = "Processing..."
-})
+const userIP = document.getElementById("userIP");
+getIPButton.addEventListener("click", () => {
+  getIPButton.innerText = "Processing...";
+});
 
 const getUserLocationInfo = async () => {
   try {
@@ -12,6 +12,7 @@ const getUserLocationInfo = async () => {
     const data = await response.json();
 
     if (data.ip) {
+      userIP.innerText = data.ip;
       const userData = await fetch(`https://ipapi.co/${data.ip}/json/`);
       const userInfo = await userData.json();
 
